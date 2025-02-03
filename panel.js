de78b2e629c29ab223308cd39aeab36629fb0dda
Tony Lea <tnylea@gmail.com>
2025-02-03 16:08:03 -0500

    Adding extension updates

--- panel.js
+++ panel.js
@@ -1,6 +1,7 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   const currentTab = tabs[0];
   const url = new URL(currentTab.url);
+  //const devtoolsUrl = `http://devtools.test/devtools`;
   const devtoolsUrl = `${url.origin}/devtools`;
   document.getElementById('devtoolsFrame').src = devtoolsUrl;
 });
